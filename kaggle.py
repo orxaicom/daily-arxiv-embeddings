74f62a587037b62e52c725f7fa5e008bc8253729
ORX AI <131006215+orxai@users.noreply.github.com>
2024-01-13 09:51:08 +0330

    Delete kaggle.py

--- kaggle.py
+++
@@ -1,5 +0,0 @@
-# %% [code]
-!pip install -U adapters
-!git clone "https://github.com/orxaicom/daily-arxiv-embeddings.git"
-!python ./daily-arxiv-embeddings/scrape.py
-!python ./daily-arxiv-embeddings/embed.py
