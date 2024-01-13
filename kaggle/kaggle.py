bb43912d36b86fcdb8a712d692bd23497d062fd2
ORX AI <131006215+orxai@users.noreply.github.com>
2024-01-13 09:50:51 +0330

    Create kaggle.py

---
+++ kaggle/kaggle.py
@@ -0,0 +1,5 @@
+# %% [code]
+!pip install -U adapters
+!git clone "https://github.com/orxaicom/daily-arxiv-embeddings.git"
+!python ./daily-arxiv-embeddings/scrape.py
+!python ./daily-arxiv-embeddings/embed.py
